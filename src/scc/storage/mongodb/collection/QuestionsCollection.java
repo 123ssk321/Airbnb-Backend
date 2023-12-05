8cdb97bbbd35354866a6b99e5ab2f48f1e456c33
bruno <bm.carmo@campus.fct.unl.pt>
2023-12-05 09:19:52 +0000

    Implemented users and questions mongodb collections

--- src/scc/storage/mongodb/collection/QuestionsCollection.java
+++ src/scc/storage/mongodb/collection/QuestionsCollection.java
@@ -1,11 +1,16 @@
 package scc.storage.mongodb.collection;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import org.bson.BsonType;
 import org.bson.conversions.Bson;
 import scc.data.dao.QuestionDAO;
 import scc.storage.QuestionsStorage;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.*;
 
 public class QuestionsCollection implements QuestionsStorage {
     private final MongoCollection<QuestionDAO> collection;
@@ -16,30 +21,41 @@ public class QuestionsCollection implements QuestionsStorage {
 
     @Override
     public QuestionDAO putQuestion(QuestionDAO question) {
+        if(collection.insertOne(question).wasAcknowledged())
+            return question;
         return null;
     }
 
     @Override
     public QuestionDAO getQuestion(String questionId) {
-        return null;
+        return collection.find(eq("id", questionId)).first();
     }
 
     @Override
     public boolean hasQuestion(String questionId) {
-        return false;
+        return this.getQuestion(questionId) != null;
     }
 
     public void addReply(String questionId, Bson update){
-
+        collection.updateOne(eq("id", questionId), update);
     }
 
     @Override
     public boolean hasReply(String questionId) {
-        return false;
+        var question = this.getQuestion(questionId);
+        return question != null && question.getReply() != null;
     }
 
     @Override
     public List<QuestionDAO> getHouseQuestions(String houseId, Boolean answered, int start, int length) {
-        return null;
+        FindIterable<QuestionDAO> query = null;
+        if(answered == null){
+            query = collection.find(eq("houseId", houseId));
+        } else if (answered) {
+            query = collection.find(and(eq("houseId", houseId), ne("reply", null)));
+        } else {
+            query = collection.find(and(eq("houseId", houseId), type("reply", BsonType.NULL)));
+        }
+        return query.skip(start).limit(length).into(new ArrayList<>());
     }
 }
--- src/scc/storage/mongodb/collection/UsersCollection.java
+++ src/scc/storage/mongodb/collection/UsersCollection.java
@@ -1,11 +1,17 @@
 package scc.storage.mongodb.collection;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
 import org.bson.conversions.Bson;
 import scc.data.dao.UserDAO;
 import scc.storage.UsersStorage;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.empty;
+import static com.mongodb.client.model.Filters.eq;
 
 public class UsersCollection implements UsersStorage {
     private final MongoCollection<UserDAO> collection;
@@ -16,30 +22,32 @@ public class UsersCollection implements UsersStorage {
 
     @Override
     public UserDAO putUser(UserDAO user) {
+        if(collection.insertOne(user).wasAcknowledged())
+            return user;
         return null;
     }
 
     @Override
     public UserDAO getUser(String userId) {
-        return null;
+        return collection.find(eq("id", userId)).first();
     }
 
     @Override
     public List<UserDAO> getUsers() {
-        return null;
+        return collection.find(empty()).into(new ArrayList<>());
     }
 
     public UserDAO updateUser(String userId, Bson updates){
-        return null;
+        return collection.findOneAndUpdate(eq("id", userId), updates, new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
     }
 
     @Override
     public void delUserById(String id) {
-
+        collection.deleteOne(eq("id", id));
     }
 
     @Override
     public void delUser(UserDAO user) {
-
+        collection.deleteOne(eq("id", user.getId()));
     }
 }
